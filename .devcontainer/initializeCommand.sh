d93e3b5538752f38aa4f2acf6edfa5d571ac1447
Lars Kruse <lakruzz@inc-inc.dk>
2025-12-22 18:39:30 +0000

    Codespace - resolves #31
    dde0467: codespace is done - #31
    
    47edaa9: codespace - #31

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-
-set -e
-
 PREFIX="👀  "
-echo "$PREFIX Running $(basename $0)"
 
-echo "$PREFIX Initializing  GH CLI"
+if [[ "$CODESPACES" == "true" ]]; then
+  # Running in GitHub CodeSpaces
+  echo "$PREFIX Running in GitHub Codespaces - skipping initialization script"
+else
+  set -e
+  
+  echo "$PREFIX Running $(basename $0)"
+  
+  echo "$PREFIX Initializing  GH CLI"
+  
+  $(dirname $0)/gh-login.sh initialize
+  
+  echo "$PREFIX SUCCESS"
+  exit 0
+fi
 
-$(dirname $0)/gh-login.sh initialize
-
-echo "$PREFIX SUCCESS"
-exit 0
 
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,22 +6,33 @@ PREFIX="🍰  "
 echo "$PREFIX Running $(basename $0)"
 
 echo "$PREFIX Setting up safe git repository to prevent dubious ownership errors"
-git config --global --add safe.directory /workspace
+if [[ "$CODESPACES" == "true" ]]; then
+  git config --global --add safe.directory /workspaces/devopsdays.dk
+else
+  git config --global --add safe.directory /workspace
+fi
 
 echo "$PREFIX Setting up git configuration to support .gitconfig in repo-root"
 git config --local --get include.path | grep -e ../.gitconfig >/dev/null 2>&1 || git config --local --add include.path ../.gitconfig
 
-
-# Check if the GH CLI is required
-if [ -e $(dirname $0)/_temp.token ]; then
-    echo "$PREFIX setting up GitHub CLI"
-    $(dirname $0)/gh-login.sh postcreate
-    echo "$PREFIX Installing the devx-cafe/gh-tt gh cli extension"
-    gh extension install devx-cafe/gh-tt
-    echo "$PREFIX Installing the gh aliases"    
-    gh alias import .devcontainer/.gh_alias.yml --clobber
-
+if [[ "$CODESPACES" == "true" ]]; then
+  echo "$PREFIX Running in GitHub Codespaces - disabling commit signing"
+  git config --global commit.gpgsign false
+  echo "$PREFIX Running in GitHub Codespaces - skipping GH CLI setup"
+else
+  if [ -e $(dirname $0)/_temp.token ]; then
+      echo "$PREFIX setting up GitHub CLI"
+      $(dirname $0)/gh-login.sh postcreate
+  fi 
 fi
+
+echo "$PREFIX Installing the devx-cafe/gh-tt gh cli extension"
+gh extension install devx-cafe/gh-tt
+echo "$PREFIX Installing the gh aliases"    
+gh alias import .devcontainer/.gh_alias.yml --clobber
+
+echo "$PREFIX Refreshing GitHub CLI scopes to include project access"
+gh auth refresh -s project
 
 echo "$PREFIX Installing the Ruby gems"
 bundle config set frozen true # use the existing Gemfile.lock
