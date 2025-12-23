fb13e0a72bd16e92809b71f887be8ddf1cf28457
Lars Kruse <lakruzz@inc-inc.dk>
2025-12-23 15:57:32 +0000

    Add codespace devcontainer - resolves #34
    7318fdd: setup a gho token - #34

--- .devcontainer/codespace/postCreateCommand.sh
+++ .devcontainer/codespace/postCreateCommand.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# This post-create command script is designed for GitHub Codespaces that uses TakT
+# It sets up git configuration, logs into GitHub CLI using a provided token,
+# You should generate the GitHub OAuth token in a shell like this:
+#
+# $ gh auth login --web --hostname github.com --git-protocol https
+#
+# hereafter you can get the toke with:
+#
+# $ gh auth token
+#
+# Copy the token and got to https://github.com/settings/codespaces and store it as a secret named TAKT_GHO
+
 set -e
 
 PREFIX="🍰  "
@@ -19,21 +31,25 @@ git config --local --get include.path | grep -e ../.gitconfig >/dev/null 2>&1 ||
 echo "$PREFIX Running in GitHub Codespaces - disabling commit signing"
 git config --local commit.gpgsign false
 
-echo "$PREFIX Use defined token if it exists"
-if [ -n "$TAKT_GITHUB_TOKEN" ]; then
-  echo "$PREFIX 🔍 Found it! Logging into GitHub CLI using \$TAKT_GITHUB_TOKEN" 
-  unset GITHUB_TOKEN
-  gh auth login --with-token <<< "$TAKT_GITHUB_TOKEN"
-fi
-
 echo "$PREFIX Installing the devx-cafe/gh-tt gh cli extension"
-gh extension install devx-cafe/gh-tt
+gh extension install devx-cafe/gh-tt --pin experimental
 echo "$PREFIX Installing the gh aliases"    
 gh alias import .devcontainer/.gh_alias.yml --clobber
 
 echo "$PREFIX Installing the Ruby gems"
 bundle config set frozen true # use the existing Gemfile.lock
 bundle install
+
+echo "$PREFIX Use defined token if it exists"
+if [ -n "$TAKT_GHO" ]; then
+  echo "$PREFIX 🔍 Found it! Logging into GitHub CLI using \$TAKT_GHO" 
+  unset GITHUB_TOKEN
+  gh auth login --with-token <<< "$TAKT_GHO"
+else
+  echo "$PREFIX 🤷‍♂️ No \$TAKT_GHO found - you need to login manually:"
+  echo "   unset GITHUB_TOKEN"
+  echo "   gh auth login --web --hostname github.com --git-protocol https"
+fi  
 
 echo "$PREFIX SUCCESS"
 exit 0
